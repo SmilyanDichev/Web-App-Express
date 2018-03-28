d9a9a1509b83af38a86428c5e46e4bca1e99a5d7
de.angelov.bg@gmail.com <de.angelov.bg@gmail.com>
2018-03-28 12:36:51 +0300

    setuped local storage and linked it to cart badge

---
+++ public/script/cart.js
@@ -0,0 +1,58 @@
+$(function () {
+    const addToStorage = function (order, counter) {
+        const localStorageSelector = "clientOrders";
+        const lastStorage = [ order ];
+        const currentStoredObject = localStorage.getItem(localStorageSelector);
+        const currentStoredObjectJSON = JSON.parse(currentStoredObject);
+        const currentStorage = currentStoredObjectJSON.storage;
+        currentStorage.push(lastStorage);
+        clientOrders = {
+            counter: counter,
+            storage: currentStorage
+        };
+        localStorage.setItem(
+            localStorageSelector, JSON.stringify(clientOrders)
+        );
+    };
+
+    const setCounterStorage = function (currentStoredObject, localStorageSelector) {
+        if (currentStoredObject === null) {
+            const clientOrders = {
+                counter: 0,
+                storage: []
+            };
+            localStorage.setItem(
+                localStorageSelector, JSON.stringify(clientOrders)
+            );
+            return 0;
+        }
+            const currentStoredObjectJSON = JSON.parse(currentStoredObject);
+            return currentStoredObjectJSON.counter;
+    };
+
+    const addCounter = function () {
+        itemsCart.forEach((item) => {
+            $(item).text(counter);
+        });
+    };
+    let clientOrders = localStorage.getItem("clientOrders");
+    const products = $(".product-wrapper").toArray();
+    const itemsCart = $(".items-basket").toArray();
+    let counter = setCounterStorage(clientOrders, "clientOrders");
+    addCounter();
+    products.forEach((item) => {
+        const name = $(item).find("h2").first().text();
+        const button = $(item).find("a:contains('Add to basket')").first();
+        const number = button.siblings().first().val();
+        const order = {
+            name: name,
+            number: number
+        };
+
+        $(button).click(function (e) {
+            counter += +number;
+            addCounter();
+            addToStorage(order, counter);
+        });
+    });
+});
