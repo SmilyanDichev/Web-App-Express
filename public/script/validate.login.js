ff1e858c20b85cb7bb424c54210c0d8f73d3cfb5
Smilyan Dichev <dahaca6022@gmail.com>
2018-04-01 12:53:51 +0300

    fixed clientside validation

--- public/script/validate.login.js
+++ public/script/validate.login.js
@@ -1,42 +1,70 @@
 /* globals $ */
 $(function() {
-    const $email = $("#loginFormEmail");
-    const $password = $("#loginFormPassword");
-    const $submitBtn = $("#loginFormSubmit");
+    const $email = $("#loginEmail");
+    const $password = $("#loginPassword");
     const $clearBtn = $("#loginFormClose");
-
+    const $loginAnchor = $("#loginAnchor");
 
     const $emailError = $("#loginFormMailError");
     const $passwordError = $("#loginFormPasswordError");
 
+
+    $loginAnchor.click(function() {
+        $email.val("");
+        $password.val("");
+        $passwordError.text("");
+        $emailError.text("");
+        $("#loginFormUserValidationError").text("");
+    });
+
     $clearBtn.on("click", function() {
         $email.val("");
         $password.val("");
-        $passwordError.hide();
-        $emailError.hide();
+        $passwordError.text("");
+        $emailError.text("");
+        $("#loginFormUserValidationError").text("");
     });
-    $submitBtn.on("click", function() {
-        let isEmail = false;
-        let isPass = false;
-        if ($email.val() === "") {
-            $emailError.show();
-        } else {
-            $emailError.hide();
+
+    const validateEmail = (mail) => {
+     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+        return true;
+      }
+        return false;
+    };
+
+    let isEmail = false;
+    let isPass = false;
+
+    $email.focusout(function () {
+        if (validateEmail($email.val())) {
+            $emailError.text("");
             isEmail = true;
-        }
-
-        if ($password.val() === "") {
-            $passwordError.show();
+        } else if ($email.val() === "") {
+            $emailError.text("Email required!");
+            isEmail = false;
         } else {
-            $passwordError.hide();
-            isPass = true;
-        }
-
-        if (isEmail && isPass) {
-
-            // send to server
-            $email.val("");
-            $password.val("");
+            $emailError.text("Invalid email!");
+            isEmail = false;
         }
     });
+    $password.focusout(function () {
+        if ($password.val() !== "") {
+            $passwordError.text("");
+            isPass = true;
+        } else {
+            $passwordError.text("Password required!");
+            isPass = false;
+        }
+    });
+
+    const isValidLogin = function() {
+        if (isEmail && isPass) {
+            return true;
+        }
+        return false;
+    };
+
+    module.exports = {
+        isValidLogin
+    };
 });
